1069e72cd96aff6383daaec14c9390185a469159
SergeyLobanov <sinet_ua@bk.ru>
2016-08-15 14:49:38 +0300

    use Mockito at JUnit

--- src/test/ua/kpi/model/entities/CourseMemberTest.java
+++ src/test/ua/kpi/model/entities/CourseMemberTest.java
@@ -3,11 +3,9 @@ package test.ua.kpi.model.entities;
 import ua.kpi.model.entities.Course;
 import ua.kpi.model.entities.CourseMember;
 import ua.kpi.model.entities.Student;
-import ua.kpi.model.entities.Teacher;
 
 import static org.junit.Assert.*;
-
-import java.util.Date;
+import static org.mockito.Mockito.*;
 
 import org.junit.After;
 import org.junit.Before;
@@ -19,24 +17,18 @@ import org.junit.Test;
  * Created by Сергей on 12.08.2016.
  */
 public class CourseMemberTest {
-    Teacher teacher;
-    Course course;
-    Student student;
+	
     CourseMember courseMember;
 
     @Before
-    public void setUpCourseMember() {
-        teacher = new Teacher(1, "Teacher", "teacherLogin", "teacherPassword");
-        course = new Course(1, "Course", teacher, new Date(), new Date());
-        student = new Student(1, "Name", "studentLogin", "studentPassword");
+    public void setUp() {
+    	Course course = mock(Course.class);
+    	Student student = mock(Student.class);
         courseMember = new CourseMember(1, course, student, 5, "comment");
     }
 
     @After
     public void tearDown() {
-        teacher = null;
-        student = null;
-        course = null;
         courseMember = null;
     }
     
